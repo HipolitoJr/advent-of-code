78899a94519ed7be7e9cf9685ca45de4189043da
hipolito.junior <hipolito.junior@etipi.pi.gov.br>
2025-12-02 13:32:04 -0300

    Add answer for day 01

---
+++ day01/answer.rb
@@ -0,0 +1,25 @@
+file = File.read('input.txt')
+inputs = file.split("\n")
+
+@pointer = 50
+@password = 0
+
+for input in inputs
+	rotation_direction = input.slice!(0, 1) # R ou L
+	rotation_size = input.to_i
+
+	for i in (1..rotation_size)
+		if rotation_direction == "L"
+			@pointer -= 1
+			@pointer = 99 if @pointer == -1
+		elsif rotation_direction == "R"
+			@pointer += 1
+			@pointer = 0 if @pointer == 100
+		else
+			next
+		end
+		@password += 1 if @pointer == 0
+	end
+end
+
+puts "The password using method 0x434C49434B, is: #{@password}"
